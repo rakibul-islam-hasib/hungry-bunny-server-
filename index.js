b6f69498e1f7c6578d3f96b4906b033250b4422e
Hasib Hossain <boraborhasib@gmail.com>
2023-08-16 22:26:37 +0600

    JWT updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 5000 || process.env.PORT;
+const jwt = require('jsonwebtoken');
 
 const userRouter = require('./routes/userRouter');
 // Middleware
@@ -11,6 +13,47 @@ app.use(express.json());
 app.use('/users', userRouter);
 
 // Routes
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgfriso.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+// const user = ;
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        app.post('/set-token', (req, res) => {
+            const user = req.body;
+            // console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_SECRET, { expiresIn: '24h' })
+            res.send({ token });
+        });
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
