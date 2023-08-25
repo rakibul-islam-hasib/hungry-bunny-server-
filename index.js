ea706fa1953803aa44872273b1422a21c8205b0f
Hasib Hossain <boraborhasib@gmail.com>
2023-08-25 21:59:42 +0600

    hjsasd

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
         const database = client.db('hungry_bunny');
         const usersCollection = database.collection('users');
         const communityPostCollection = database.collection('community_post');
