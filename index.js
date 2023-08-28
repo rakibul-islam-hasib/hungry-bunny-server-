aabf28ee6c8b124c909b083b47802a4f72ac12cc
tufaelkhan <tufaelkhan247@gmail.com>
2023-08-28 03:53:05 -0700

    add new id base blog route

--- index.js
+++ index.js
@@ -58,6 +58,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/blogs/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await blogsCollection.findOne(query)
+            res.send(result)
+        })
+
         app.post('/set-token', (req, res) => {
             const user = req.body;
             // console.log(user)
