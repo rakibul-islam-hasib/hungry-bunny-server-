67a10c0fee6baea80a257753fff46d8e687727e3
Hasib Hossain <boraborhasib@gmail.com>
2023-08-30 22:40:17 +0600

    Restaurent search added

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -7,12 +7,26 @@ router.get('/', async (req, res) => {
     const result = await cursor.toArray()
     res.send(result)
 })
-
+router.get('/total/count', async (req, res) => {
+    const cursor = req.mongo.restaurantCollection.estimatedDocumentCount()
+    const result = await cursor;
+    res.send({ total: result })
+})
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
     // console.log(id);
     const query = { _id: new ObjectId(id) }
     const result = await req.mongo.restaurantCollection.findOne(query);
+    res.send(result)
+})
+// Search by name
+router.get('/search/query', async (req, res) => {
+    // const name = req..name;
+    const name = req.query.name;
+    console.log(name)
+    const query = { restaurantName: { $regex: name, $options: 'i' } }
+    const cursor = req.mongo.restaurantCollection.find(query)
+    const result = await cursor.toArray()
     res.send(result)
 })
 
