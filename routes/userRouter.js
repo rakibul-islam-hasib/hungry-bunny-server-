5d361aa0de876cf47aa0440e94aa1028a1f80db7
Hasib Hossain <boraborhasib@gmail.com>
2023-08-31 22:29:37 +0600

    updated

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -39,4 +39,14 @@ router.get('/post/:email', verifyJWT, async (req, res) => {
     const result = await req.mongo.usersCollection.findOne({ email: email }, { projection: { post: 1, _id: 0 } });
     res.send(result.post);
 });
+
+
+// Get user email , 
+router.get('/email/:email', verifyJWT, async (req, res) => {
+    const email = req.params.email;
+    const regex = new RegExp(email, 'i'); // i for case insensitive
+    const result = await req.mongo.usersCollection.findOne({ email: regex }, { projection: { email: 1, _id: 0 } });
+    res.send(result);
+});
+
 module.exports = router;
