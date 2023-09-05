dc576f97282efba70e64653c7099d4b4498cf736
Hasib Hossain <boraborhasib@gmail.com>
2023-09-05 23:49:19 +0600

    application route updated

--- routes/applicationsRoute.js
+++ routes/applicationsRoute.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const verifyJWT = require('../middleware/verifyJWT');
 
-router.post('/apply', async (req, res) => {
+router.post('/apply', verifyJWT, async (req, res) => {
     const data = req.body;
     const applicationCollection = req.mongo.applicationCollection;
     const result = await applicationCollection.insertOne(data);
     res.send(result);
 });
+router.get('/get', verifyJWT, async (req, res) => {
+    const applicationCollection = req.mongo.applicationCollection;
+    const result = await applicationCollection.find({}).toArray();
+    res.send(result);
+});
+
+
+
 
 module.exports = router;
