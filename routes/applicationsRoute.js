d59be6a090a970b9190093355f011536b2a84edf
Hasib Hossain <boraborhasib@gmail.com>
2023-09-05 23:52:14 +0600

    More update changed

--- routes/applicationsRoute.js
+++ routes/applicationsRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const verifyJWT = require('../middleware/verifyJWT');
+const { ObjectId } = require('mongodb');
 
 router.post('/apply', verifyJWT, async (req, res) => {
     const data = req.body;
@@ -13,7 +14,16 @@ router.get('/get', verifyJWT, async (req, res) => {
     const result = await applicationCollection.find({}).toArray();
     res.send(result);
 });
-
+router.put('/approved/:id', verifyJWT, async (req, res) => {
+    const applicationCollection = req.mongo.applicationCollection;
+    const { status } = req.body;
+    const filter = { _id: new ObjectId(req.params.id) }
+    const docx = {
+        $set: { status: status }
+    }
+    const result = await applicationCollection.updateOne(filter, docx);
+    res.send(result);
+});
 
 
 
