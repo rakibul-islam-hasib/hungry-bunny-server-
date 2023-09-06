c17782dd6d11e6f46935885441fd01f04f18d8fa
Hasib Hossain <boraborhasib@gmail.com>
2023-09-06 11:49:33 +0600

    Application approving system added

--- routes/applicationsRoute.js
+++ routes/applicationsRoute.js
@@ -16,13 +16,22 @@ router.get('/get', verifyJWT, async (req, res) => {
 });
 router.put('/status/:id', verifyJWT, async (req, res) => {
     const applicationCollection = req.mongo.applicationCollection;
-    const { status } = req.body;
+    const userCollection = req.mongo.usersCollection;
     const filter = { _id: new ObjectId(req.params.id) }
+    const userId = await applicationCollection.findOne(filter, { projection: { userId: 1, _id: 0 } });
+    const { status, userRole } = req.body;
+    const userFilter = { _id: new ObjectId(userId.userId) }
     const docx = {
         $set: { status: status }
     }
+    const userDocx = {
+        $set: { role: userRole }
+    }
+    // console.log(userId);
+    // now find the user and update the role
+    const userResult = await userCollection.updateOne(userFilter, userDocx);
     const result = await applicationCollection.updateOne(filter, docx);
-    res.send(result);
+    res.send({ result, userResult });
 });
 
 
