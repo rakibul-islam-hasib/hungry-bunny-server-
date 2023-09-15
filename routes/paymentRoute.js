1a40691d29ac831ed35b85ac49ca4a5b57c3c53b
Hasib Hossain <boraborhasib@gmail.com>
2023-09-15 18:45:35 +0600

    Payment intent done

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const verifyJWT = require('../middleware/verifyJWT');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-router.post('/create-payment-intent', async (req, res) => {
-    const { amount } = req.body;
-    try {
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: Number(amount) * 100,
-            currency: 'usd',
-            payment_method_types: ['card']
-        });
-        res.send({ clientSecret: paymentIntent.client_secret });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+router.post('/create-payment-intent', verifyJWT, async (req, res) => {
+    const { price } = req.body;
+    if (!price || price === 0 || price == null) {
+        return res.json({ error: 'Missing price in request body' });
     }
-});
+    const amount = parseInt(price) * 100;
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+    });
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    });
+})
 
 
 module.exports = router;
