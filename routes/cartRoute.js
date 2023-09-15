4f31a5cbc2903e56549d83ad6fc1e6c6abdf4e4c
Hasib Hossain <boraborhasib@gmail.com>
2023-09-15 11:24:52 +0600

    Pipeline পোকা মাকোর ফিকস

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -34,7 +34,7 @@ router.get('/in-cart/:userId', async (req, res) => {
             }
         },
         {
-            $unwind: '$foodDetails' 
+            $unwind: '$foodDetails'
         },
         {
             $lookup: {
@@ -45,7 +45,7 @@ router.get('/in-cart/:userId', async (req, res) => {
             }
         },
         {
-            $unwind: '$restaurantDetails' 
+            $unwind: '$restaurantDetails'
         },
         {
             $lookup: {
@@ -56,20 +56,20 @@ router.get('/in-cart/:userId', async (req, res) => {
             }
         },
         {
-            $unwind: '$userDetails' // Unwind the userDetails array (if there are multiple matches)
+            $unwind: '$userDetails'
         },
         {
             $group: {
-                _id: '$foodDetails._id', // Group by foodDetails._id
-                foodDetails: { $first: '$foodDetails' }, // Take the foodDetails from the first document in the group
-                restaurantDetails: { $first: '$restaurantDetails' }, // Take the restaurantDetails from the first document in the group
-                userDetails: { $first: '$userDetails' }, // Take the userDetails from the first document in the group
-                quantity: { $sum: 1 } // Count the number of documents with the same itemId
+                _id: '$foodDetails._id',
+                foodDetails: { $first: '$foodDetails' },
+                restaurantDetails: { $first: '$restaurantDetails' },
+                userDetails: { $first: '$userDetails' },
+                quantity: { $sum: 1 }
             }
         },
         {
             $project: {
-                _id: 0, // Exclude the _id field
+                _id: 1,
                 'foodDetails.food_name': 1,
                 'foodDetails.category': 1,
                 'foodDetails.image': 1,
@@ -78,7 +78,7 @@ router.get('/in-cart/:userId', async (req, res) => {
                 'restaurantDetails.restaurant_name': 1,
                 'userDetails.name': 1,
                 'userDetails.email': 1,
-                quantity: 1 // Include the quantity field with the count
+                quantity: 1
             }
         }
     ];
